9ed0e43391ab21f28bdd19c830f2fafd92735a66
Sanchit Kalra <sanchit.kalra1@gmail.com>
2021-04-24 16:18:48 +0530

    Update BASE_URL to env var

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -6,8 +6,14 @@ import {
     FormField,
     MaskedInput,
     TextInput,
-    Text
+    Text,
+    Header,
+    Anchor
   } from 'grommet';
+
+import { LinkPrevious } from 'grommet-icons';
+
+import { BASE_URL } from '../../constants'
 
 function verifyEmailValidity(email) {
     var reg = /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(hyderabad.bits-pilani)\.ac.in$/
@@ -33,35 +39,43 @@ function Auth() {
     }
 
     return(
-        <Box fill align="center" justify="center">
-            <Box width="medium">
-                <Form>
-                    <FormField label="Email" name="email" required>
-                        <MaskedInput
-                            value={email}
-                            onChange={e => {
-                                setEmail(e.target.value)
-                            }}
-                            name="email"
-                            type="email"
-                        />
-                    </FormField>
-                    <FormField label="Password" name="password">
-                        <TextInput name="password" type="password"/>
-                    </FormField>
-                    <Box direction="row" justify="between" margin={{ top: 'medium' }}>
-                        <Button onClick={submitForm} type="submit" label="Login" primary />
-                        <Button type="reset" label="Create Account" />
-                    </Box>
-                    <Box direction="row" justify="between" margin={{ top: 'medium' }}>
-                        <Button type="reset" size="small" label="Reset Password" />
-                    </Box>
-                    <Box direction="row" justify="between" margin={{ top: 'medium' }}>
-                        {
-                            emailError ? <Text></Text> : <Text>only BITS Hyderabad email addresses</Text>
-                        }
-                    </Box>
-                </Form>
+        <Box>
+            <Header pad="medium" height="xsmall">
+            <Anchor
+                href={BASE_URL}
+                icon={<LinkPrevious color="brand" />}
+            />
+            </Header>
+            <Box fill align="center" justify="center">
+                <Box width="medium">
+                    <Form>
+                        <FormField label="Email" name="email" required>
+                            <MaskedInput
+                                value={email}
+                                onChange={e => {
+                                    setEmail(e.target.value)
+                                }}
+                                name="email"
+                                type="email"
+                            />
+                        </FormField>
+                        <FormField label="Password" name="password">
+                            <TextInput name="password" type="password"/>
+                        </FormField>
+                        <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+                            <Button onClick={submitForm} type="submit" label="Login" primary />
+                            <Button type="reset" label="Create Account" />
+                        </Box>
+                        <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+                            <Button type="reset" size="small" label="Reset Password" />
+                        </Box>
+                        <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+                            {
+                                emailError ? <Text></Text> : <Text>only BITS Hyderabad email addresses</Text>
+                            }
+                        </Box>
+                    </Form>
+                </Box>
             </Box>
         </Box>
     )
