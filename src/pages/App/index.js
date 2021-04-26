0272cc7959ee61ef8c7b9a3fac5836f4a09c1368
Sanchit Kalra <sanchit.kalra1@gmail.com>
2021-04-26 15:33:31 +0530

    Added logout functionality

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,9 +1,62 @@
 import React from 'react'
 
-function Dashboard() {
-    return(
-        <p>Dashboard</p>
+import { Header, Anchor, Box, Button, Heading, Form, FormField, MaskedInput } from 'grommet';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router'
+
+import firebaseRef from '../../firebaseRef'
+
+function Dashboard (props) {
+
+    let history = useHistory()
+
+    function onLogoutPress (e) {
+        firebaseRef.auth().signOut().then(() => {
+            // Sign-out successful.
+            history.push('/')
+          }).catch((error) => {
+            // An error happened.
+          });
+    }
+
+    return (
+        <Box>
+            <Header background="light-4" pad="medium" height="xsmall">
+                <Box>
+                    <Link to='/app'>
+                        <Anchor
+                            label="Dashboard"
+                        />
+                    </Link>
+                </Box>
+                <Box justify="end" direction="row" gap="medium">
+                    <Button
+                        onClick={onLogoutPress}
+                        label="Logout"
+                    />
+                </Box>
+            </Header>
+            <Box pad="xlarge">
+                <Heading level="3" alignSelf="center">
+                    Enter a URL to shorten
+                </Heading>
+                <Form>
+                    <FormField label="URL" name="url" required>
+                        <MaskedInput
+                            name="url"
+                        />
+                    </FormField> 
+                    <br />
+                    <FormField label="Shortcode" name="shortcode" required>
+                        <MaskedInput
+                            name="shortcode"
+                        />
+                    </FormField>             
+                </Form>
+            </Box>
+        </Box>
     )
+
 }
 
 export default Dashboard
